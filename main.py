74a9e8b8810d804f56ecc86cebc2b6a3468c93eb
Mauricio Gonzalez <dreg5000.bbb@gmail.com>
2022-05-14 13:31:13 -0400

    Path_Operation_Structures_Tweets

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Python
+from email.policy import HTTP
 from typing import List, Dict
 
 # Models
@@ -12,24 +13,9 @@ app = FastAPI()
 
 # Path Operations
 
-@app.get(
-    path='/',
-    tags=['Home'])
-def home() -> Dict[str, str]:
-    """
-    Home Route
-
-    Show a message explaining if Twitter API is working
-
-    Parameters:
-    - None
-
-    Returns JSON key: 'Twitter API' and JSON value: 'Working'
-    """
-    return {'Twitter API': 'Working'}
-
 ## Users
 
+### Register a user
 @app.post(
     path='auth/singup',
     response_model=User,
@@ -42,6 +28,8 @@ def singup(
 ):
     pass
 
+
+### Login a User
 @app.post(
     path='auth/login',
     response_model=User,
@@ -54,6 +42,8 @@ def login(
 ):
     pass
 
+
+### Show all users
 @app.get(
     path='/users',
     response_model=List[User],
@@ -66,6 +56,8 @@ def show_all_users(
 ):
     pass
 
+
+### Show a user
 @app.get(
     path='/users/{user_id}',
     response_model=User,
@@ -78,6 +70,8 @@ def show_a_users(
 ):
     pass
 
+
+### Delete a user
 @app.delete(
     path='/users/{user_id}/delete',
     response_model=User,
@@ -90,6 +84,8 @@ def delete_a_user(
 ):
     pass
 
+
+### Update a User
 @app.put(
     path='/users/{user_id}/update',
     response_model=User,
@@ -103,3 +99,72 @@ def Update_a_user(
     pass
 
 ## Tweets
+
+### Post a tweet
+
+@app.post(
+    path='/post',
+    response_model=Tweet,
+    status_code=status.HTTP_201_CREATED,
+    summary='Post a tweet',
+    tags=['Tweets']
+)
+def post():
+    pass
+
+
+### Show all tweets
+@app.get(
+    path='/',
+    response_model=List[Tweet],
+    status_code=status.HTTP_200_OK,
+    summary='Show all tweets',
+    tags=['Tweets','Home'])
+def home() -> Dict[str, str]:
+    """
+    Home Route
+
+    Show a message explaining if Twitter API is working
+
+    Parameters:
+    - None
+
+    Returns JSON key: 'Twitter API' and JSON value: 'Working'
+    """
+    return {'Twitter API': 'Working'}
+
+
+### Show a Tweet
+@app.get(
+    path='/tweets/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Show a tweet',
+    tags=['Tweets']
+)
+def show_a_tweet():
+    pass
+
+
+### Delete a tweet
+@app.delete(
+    path='/tweets/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Delete a tweet',
+    tags=['Tweets']
+)
+def delete_a_tweet():
+    pass
+
+
+### Update a Tweet
+@app.put(
+    path='/tweets/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Update a tweet',
+    tags=['Tweets']
+)
+def update_a_tweet():
+    pass
